60f5c83f37281b029970a84866983f72868dbdcd
Eimantas Stankevicius <estankevicius@live.com>
2019-03-27 13:45:16 +0800

    Added first task.

---
+++ Refactoring.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Refactoring.Core.Task_1.Before;
+
+namespace Refactoring.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager.Start();
+        }
+    }
+}
